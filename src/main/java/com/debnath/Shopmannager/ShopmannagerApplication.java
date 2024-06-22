19e36e81de923f702875877e94423b9d11efee09
kausikdebnath36 <kausikdebnath1920@gmail.com>
2024-06-22 23:31:13 +0530

    First commit pushing the base code to repo

---
+++ src/main/java/com/debnath/Shopmannager/ShopmannagerApplication.java
@@ -0,0 +1,13 @@
+package com.debnath.Shopmannager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopmannagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopmannagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/debnath/Shopmannager/ShopmannagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debnath.Shopmannager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopmannagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
